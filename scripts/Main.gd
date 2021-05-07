21a9476bd1b376809781e045fbc5e68bd90a353e
ADumas <alejandro.dumas.barrera@gmail.com>
2021-05-06 20:10:00 -0400

    Añadiendo timer kick, cambiar sprites para cada player

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -11,11 +11,13 @@ func _ready() -> void:
 		var player = Player.instance()
 		player.init(nid)
 		player.global_position = $Positions.get_child(Game.players[nid]["slot"]).global_position
+		#player.set_text($Textures/Sprite2.texture)
+		player.set_text($Textures.get_child(Game.players[nid]["slot"]).texture)
 		$Players.add_child(player)
 
 func _end_game_id(id):
 	_end_game()
-	
+
 func _end_game():
 	Game.call_deferred("end_game")
 	get_tree().change_scene("res://scenes/Lobby.tscn")
